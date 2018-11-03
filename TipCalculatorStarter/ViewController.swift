f9e6ba9b1d9ff22eb99714dc338f8522e4a0c993
noah <noahwoodward2244@gmail.com>
2018-11-03 14:25:27 -0700

    Grrrr AutoLayout

--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -11,18 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     // MARK: - View Lifecycle
-    @IBOutlet weak var billInputView: UIView!
-    @IBOutlet weak var billAmountTextLabel: UILabel!
-    @IBOutlet weak var billTextField: UITextField!
-    @IBOutlet weak var tipPercentLabel: UILabel!
-    @IBOutlet weak var tipOutputView: UIView!
-    @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        billInputView.layer.cornerRadius = 8
-        billInputView.layer.masksToBounds = true
-        tipOutputView.layer.borderWidth = 1
         
     }
     
